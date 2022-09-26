9b96c09d6cfcbd85fdf7ac91a81542a6e4a98a70
udkpg <113385643+udkpg@users.noreply.github.com>
2022-09-26 22:04:25 +0200

    Update snub.lua

--- snub.lua
+++ snub.lua
@@ -1,3 +1,3 @@
 -- Latest version of SNUB v386
 
-loadstring(game:HttpGetAsync("https://udkpg.github.io/snub/snub_version/snub_386.lua"))()
+loadstring(game:HttpGetAsync("https://udkpg.github.io/snub/snub_version/snub_388c.lua"))()
